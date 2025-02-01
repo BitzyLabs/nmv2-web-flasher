e7e88df193c37552ecc4aeee892a7c99b9b81e35
geuv <geuv@local.com>
2025-02-01 10:22:33 +0100

    changed path name in next.config for github pages

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  ...(process.env.NODE_ENV === 'production' ? {
-    basePath: '/bitaxe-web-flasher',
-    assetPrefix: '/bitaxe-web-flasher',
-  } : {}),
+  ...(process.env.NODE_ENV === 'production'
+    ? {
+        basePath: '/bitmaker-web-flasher',
+        assetPrefix: '/bitmaker-web-flasher',
+      }
+    : {}),
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
